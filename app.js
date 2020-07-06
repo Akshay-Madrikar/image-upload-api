4708eb9fde7e97c62058119b86def833bcd89290
Akshay Madrikar <akshaymadrikar@gmail.com>
2020-07-06 20:58:49 +0530

    created user model to store users with profile pic. Also intialized client-side using reactjs to perform the image-upload operation

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ require('dotenv').config();
 const upload = require('./services/multer');
 const { dataUri } = require('./services/dataUri');
 const { uploadCloudinary } = require('./services/cloudinary');
+const User = require('./model/user');
+require('./db/mongoose');
 
 const app = express();
 
@@ -21,7 +23,37 @@ const singleUploadCtrl = (req, res, next) => {
     });
 };
 
-app.post('/upload', singleUploadCtrl, async (req,res) => {
+app.get('/api/users', async(req, res) => {
+    try {
+        const users = await User.find({});
+        if(!users) {
+            throw new Error();
+        }
+        res.status(201).json(users);
+    } catch(error) {
+        res.status(400).json({
+            error: 'No users found!'
+        });
+    }
+});
+
+
+app.post('/api/create', async(req, res) => {
+    try {
+        const user = new User(req.body);
+        await user.save();
+        res.status(201).json({
+            user,
+            message: 'User created successfully!'
+        });
+    } catch(error) {
+        res.status(400).json({
+            error: 'Cannot create user!'
+        });
+    }
+});
+
+app.post('/api/upload', singleUploadCtrl, async (req,res) => {
     try {
         if(!req.file) {
             throw new Error();
